a751d2d0d547ef5516315720099cb88e4b0249f6
Varun Vora <varunvora@live.com>
2017-05-16 01:18:27 +0530

    Renamed functions, Added measuring a qubit
    The qubit is destroyed when you try to measure it. The measurement returns 0 or 1 whose probabilities are alpha^2 and beta^2 respectively.

--- Q.py
+++ Q.py
@@ -2,46 +2,62 @@
 #All function names are in camel case, and then prefixed with 'q'
 #Alpha beta Convention taken from Michael Nielsen's Youtube series 'Quantum Computing for the Determined'
 
-from random import random
+from random import random, randint
 class qubit(object) :
     def __init__(self) :
         self.alpha = random()
         self.beta = (1 - self.alpha**2)**0.5
+        self.measured = False
     
-    def qisValid(self, a, b) :
-        if ((a**2 + b**2) > 1) or (a < 0) or (b < 0) or (a > 1) or (b > 1):
+    def isValid(self, a, b) :
+        if ((a**2 + b**2) > 1) or (a < 0) or (b < 0) or (a > 1) or (b > 1) or self.measured:
             return False
         else :
             return True
-    def qset(self, a, b) :
-        if self.qisValid(a, b):
+    def setStates(self, a, b) :
+        if self.isValid(a, b) and not self.measured:
             self.alpha = a
             self.beta = b
             return True
         else :
             return False
-    
-    def qnot(self):
-        self.alpha, self.beta = self.beta, self.alpha
-    
-    def qgetStates(self) :
-        return self.alpha, self.beta
+    def getStates(self) :
+        #only for verification, remove in end
+        if not self.measured :
+            return self.alpha, self.beta
+        else :
+            return False
+    def negate(self):
+        if not self.measured :
+            self.alpha, self.beta = self.beta, self.alpha
 
-    def qgetAlpha(self) :
-        return self.alpha
- 
-    def qgetBeta(self) :
-        return self.beta
+    def hadamard(self) :
+        if self.measured :
+            return
+        root2 = 2**0.5
+        a = self.alpha + self.beta
+        b = self.alpha - self.beta
+        self.alpha = a/root2
+        self.beta = b/root2
+    
+    def measure(self) :
+        #not accurate probabilites
+        if not self.measured :
+            lim = 5 #10^lim will be the size of list
+            proba = int(str(self.alpha**2)[2:lim+2])
+            probb = 10**lim - proba
+            L = [0 for x in range(proba)]
+            for i in range(probb) : L.insert(randint(0, 10**lim-1), 1)
+            result = L[randint(0, 10**lim-1)]
+            self.measured = True #You lose the values of alpha and beta now
+            del self.alpha
+            del self.beta
+            return result
 
 
 a = qubit()
-print(a.qgetStates())
-a.qnot()
-print(a.qgetStates())
-a.qset(0.6, 0.8)
-print(a.qgetStates())
-a.qnot()
-print(a.qgetStates())
+print(a.getStates())
+print(a.measure())
 
 '''
 Float precision is only upto 16 decimal places which is going to be a drawback later
